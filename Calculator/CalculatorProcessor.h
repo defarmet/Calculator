fa61195a5c868921e99f17251cab168f342cc937
defarmet <jonsangster5363@gmail.com>
2022-07-18 14:54:35 -0400

    Fix Singleton.

--- Calculator/CalculatorProcessor.h
+++ Calculator/CalculatorProcessor.h
@@ -3,8 +3,14 @@
 #include "wx/wx.h"
 
 class CalculatorProcessor {
+private:
+	static CalculatorProcessor *calc;
+	CalculatorProcessor() = default;
 public:
-	static CalculatorProcessor &get_instance();
+	static CalculatorProcessor *get_instance();
 	void calculate(wxTextCtrl *display, unsigned char in_mode, unsigned char out_mode);
+
+	CalculatorProcessor(CalculatorProcessor &other) = delete;
+	void operator=(CalculatorProcessor &other) = delete;
 };
 
